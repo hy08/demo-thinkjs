51c70d3b298d876f9abadfbe473dbe45957abeed
韩雨 <2544348905@qq.com>
2019-10-02 18:32:19 +0800

    admin

---
+++ admin/create/src/layouts/UserLayout.jsx
@@ -0,0 +1,49 @@
+import { getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import DocumentTitle from 'react-document-title';
+import Link from 'umi/link';
+import React from 'react';
+import { connect } from 'dva';
+import logo from '../assets/logo.svg';
+import styles from './UserLayout.less';
+
+const UserLayout = props => {
+  const {
+    route = {
+      routes: [],
+    },
+  } = props;
+  const { routes = [] } = route;
+  const {
+    children,
+    location = {
+      pathname: '',
+    },
+  } = props;
+  const { breadcrumb } = getMenuData(routes);
+  return (
+    <DocumentTitle
+      title={getPageTitle({
+        pathname: location.pathname,
+        breadcrumb,
+        ...props,
+      })}
+    >
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <Link to="/">
+                <img alt="logo" className={styles.logo} src={logo} />
+                <span className={styles.title}>后台管理系统</span>
+              </Link>
+            </div>
+            <div className={styles.desc}>昆山三艺强包装材料有限公司</div>
+          </div>
+          {children}
+        </div>
+      </div>
+    </DocumentTitle>
+  );
+};
+
+export default connect(({ settings }) => ({ ...settings }))(UserLayout);
---
+++ admin/create/src/pages/user/login/index.jsx
@@ -0,0 +1,141 @@
+import { Alert, Checkbox } from 'antd';
+import React, { Component } from 'react';
+import Link from 'umi/link';
+import { connect } from 'dva';
+import LoginComponents from './components/Login';
+import styles from './style.less';
+const { Tab, UserName, Password, Submit } = LoginComponents;
+
+@connect(({ login, loading }) => ({
+  userLogin: login,
+  submitting: loading.effects['login/login'],
+}))
+class Login extends Component {
+  loginForm = undefined;
+  state = {
+    type: 'account',
+    autoLogin: true,
+  };
+  changeAutoLogin = e => {
+    this.setState({
+      autoLogin: e.target.checked,
+    });
+  };
+  handleSubmit = (err, values) => {
+    const { type } = this.state;
+
+    if (!err) {
+      const { dispatch } = this.props;
+      dispatch({
+        type: 'login/login',
+        payload: { ...values, type },
+      });
+    }
+  };
+  onTabChange = type => {
+    this.setState({
+      type,
+    });
+  };
+  onGetCaptcha = () =>
+    new Promise((resolve, reject) => {
+      if (!this.loginForm) {
+        return;
+      }
+
+      this.loginForm.validateFields(['mobile'], {}, async (err, values) => {
+        if (err) {
+          reject(err);
+        } else {
+          const { dispatch } = this.props;
+
+          try {
+            const success = await dispatch({
+              type: 'login/getCaptcha',
+              payload: values.mobile,
+            });
+            resolve(!!success);
+          } catch (error) {
+            reject(error);
+          }
+        }
+      });
+    });
+  renderMessage = content => (
+    <Alert
+      style={{
+        marginBottom: 24,
+      }}
+      message={content}
+      type="error"
+      showIcon
+    />
+  );
+
+  render() {
+    const { userLogin, submitting } = this.props;
+    const { status, type: loginType } = userLogin;
+    const { type, autoLogin } = this.state;
+    return (
+      <div className={styles.main}>
+        <LoginComponents
+          defaultActiveKey={type}
+          onTabChange={this.onTabChange}
+          onSubmit={this.handleSubmit}
+          onCreate={form => {
+            this.loginForm = form;
+          }}
+        >
+          <Tab key="account" tab="账户密码登录">
+            {status === 'error' &&
+              loginType === 'account' &&
+              !submitting &&
+              this.renderMessage('账户或密码错误')}
+            <UserName
+              name="userName"
+              placeholder="用户名"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入用户名!',
+                },
+              ]}
+            />
+            <Password
+              name="password"
+              placeholder="密码"
+              rules={[
+                {
+                  required: true,
+                  message: '请输入密码！',
+                },
+              ]}
+              onPressEnter={e => {
+                e.preventDefault();
+                if (this.loginForm) {
+                  this.loginForm.validateFields(this.handleSubmit);
+                }
+              }}
+            />
+          </Tab>
+          <div>
+            <Checkbox checked={autoLogin} onChange={this.changeAutoLogin}>
+              自动登录
+            </Checkbox>
+            <a
+              style={{
+                float: 'right',
+              }}
+              href=""
+            >
+              忘记密码
+            </a>
+          </div>
+          <Submit loading={submitting}>登录</Submit>
+        </LoginComponents>
+      </div>
+    );
+  }
+}
+
+export default Login;
