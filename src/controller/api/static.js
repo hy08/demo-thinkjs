9b3c6448ea1468edfe9ab11420a067b7b2ecc1a2
韩雨 <2544348905@qq.com>
2019-11-05 20:28:23 +0800

    文件上传接口修改

--- src/controller/api/static.js
+++ src/controller/api/static.js
@@ -15,18 +15,22 @@ module.exports = class extends BaseRest {
       return this.fail(1000, '请上传文件');
     }
     // path.extname获取文件后缀名，可做上传控制
-    const extname = path.extname(file.name);
-    const filename = path.basename(file.path);
-    const basename = think.md5(filename) + extname;
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
-    const savepath = '/uploads/' + year + '/' + month + '/' + basename;
-    const filepath = path.join(think.ROOT_PATH, 'www' + savepath);
-    think.mkdir(path.dirname(filepath));
-    rename(file.path, filepath);
-    let data = { fileId: savepath, localName: file.name, basename: basename, openUrl: filepath };
-    return this.success(data, '上传成功');
+    try {
+      const extname = path.extname(file.name);
+      const filename = path.basename(file.path);
+      const basename = think.md5(filename) + extname;
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+      const savepath = '/uploads/' + year + '/' + month + '/' + basename;
+      const filepath = path.join(think.ROOT_PATH, 'www' + savepath);
+      think.mkdir(path.dirname(filepath));
+      rename(file.path, filepath);
+      let data = { fileId: savepath, localName: file.name, basename: basename, openUrl: filepath };
+      return this.success(data, '上传成功');
+    } catch (error) {
+      return this.fail(500, '上传异常');
+    }
   }
 
   /**
@@ -37,12 +41,15 @@ module.exports = class extends BaseRest {
   async deleteAction() {
     const fileId = this.post('fileId');
     const filepath = path.join(think.ROOT_PATH, 'www' + fileId);
-    try {
-      fs.unlinkSync(filepath);
+    const unlink = think.promisify(fs.unlink, fs);
+    const result = await unlink(filepath).catch(err => {
+      return think.isError(err) ? err : new Error(err);
+    });
+    if (think.isError(result)) {
+      return this.fail(500, '删除文件异常');
+    } else {
       const data = { fileId: fileId }
       return this.success(data, '删除成功');
-    } catch (error) {
-      return this.fail(500, '删除文件异常');
     }
   }
 };
---
+++ src/logic/api/product.js
@@ -0,0 +1,67 @@
+module.exports = class extends think.Logic {
+  getAction() {
+    const rules = {
+      categoryCode: {
+        string: true,
+        required: true
+      },
+    };
+    const msgs = {
+      categoryCode: '商品分类名称不能为空',
+    };
+    const flag = this.validate(rules, msgs);
+    if (!flag) {
+      return this.fail(1001, '接口数据不符合要求', this.validateErrors);
+    }
+  }
+  putAction() {
+    const rules = {
+      categoryCode: {
+        string: true,
+        required: true
+      },
+      name: {
+        string: true,
+        required: true
+      },
+      introduction: {
+        string: true,
+        required: true
+      },
+    };
+    const msgs = {
+      categoryCode: '商品分类名称不能为空',
+      name: '商品名称不能为空',
+      introduction: '商品介绍不能为空',
+    };
+    const flag = this.validate(rules, msgs);
+    if (!flag) {
+      return this.fail(1001, '接口数据不符合要求', this.validateErrors);
+    }
+  }
+  postAction() {
+    const rules = {
+      categoryCode: {
+        string: true,
+        required: true
+      },
+      name: {
+        string: true,
+        required: true
+      },
+      introduction: {
+        string: true,
+        required: true
+      },
+    };
+    const msgs = {
+      categoryCode: '商品分类名称不能为空',
+      name: '商品名称不能为空',
+      introduction: '商品介绍不能为空',
+    };
+    const flag = this.validate(rules, msgs);
+    if (!flag) {
+      return this.fail(1001, '接口数据不符合要求', this.validateErrors);
+    }
+  }
+};
